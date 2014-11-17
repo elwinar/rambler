9a9edf7c2fa810ad01f4552517785113d00f6be4
Romain Baugue <romain.baugue@elwinar.com>
2014-11-18 00:06:04 +0100

    Spawned a driver system from pure awesomeness

--- migration/driver.go
+++ migration/driver.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 )
 
+type Driver interface{}
+
 var (
 	drivers map[string]Driver
 )
@@ -13,20 +15,32 @@ func init() {
 }
 
 var (
-	ErrDriverAlreadyRegistered = errors.New("driver is already registered")
+	ErrDriverAlreadyRegistered = errors.New("driver already registered")
+	ErrDriverNotRegistered = errors.New("driver not registered")
 )
 
-type Driver interface{}
-
-func Register(name string, driver Driver) error {
-	return register(name, driver, drivers)
+func RegisterDriver(name string, driver Driver) error {
+	return registerDriver(name, driver, drivers)
 }
 
-func register(name string, driver Driver, drivers map[string]Driver) error {
+func registerDriver(name string, driver Driver, drivers map[string]Driver) error {
 	if _, found := drivers[name]; found {
 		return ErrDriverAlreadyRegistered
 	}
 	
 	drivers[name] = driver
 	return nil
+}
+
+func GetDriver(name string) (Driver, error) {
+	return getDriver(name, drivers)
+}
+
+func getDriver(name string, drivers map[string]Driver) (Driver, error) {
+	driver, found := drivers[name]
+	if !found {
+		return nil, ErrDriverNotRegistered
+	}
+	
+	return driver, nil
 }
--- migration/driver_test.go
+++ migration/driver_test.go
@@ -7,22 +7,47 @@ import (
 
 type MockDriver struct{}
 
-func TestRegister(t *testing.T) {
+func TestRegisterDriver(t *testing.T) {
 	g := Goblin(t)
-	g.Describe("Register", func() {
-		g.It("Should accept drivers", func() {
+	g.Describe("RegisterDriver", func() {
+		g.It("Should register new drivers", func() {
 			drivers := make(map[string]Driver)
-			err := register("mock", MockDriver{}, drivers)
+			
+			err := registerDriver("mock", MockDriver{}, drivers)
 			g.Assert(err).Equal(nil)
+			g.Assert(drivers["mock"]).Equal(MockDriver{})
 		})
 		
-		g.It("Should not accept the same driver twice", func() {
+		g.It("Shouldn't accept the same driver twice", func() {
 			drivers := make(map[string]Driver)
-			err := register("mock", MockDriver{}, drivers)
+			
+			err := registerDriver("mock", MockDriver{}, drivers)
 			g.Assert(err).Equal(nil)
 			
-			err = register("mock", MockDriver{}, drivers)
+			err = registerDriver("mock", MockDriver{}, drivers)
 			g.Assert(err).Equal(ErrDriverAlreadyRegistered)
+		})
+	})
+}
+
+func TestGetDriver(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("GetDriver", func() {
+		g.It("Should retrieve existing drivers", func() {
+			drivers := make(map[string]Driver)
+			drivers["mock"] = MockDriver{}
+			
+			driver, err := getDriver("mock", drivers)
+			g.Assert(err).Equal(nil)
+			g.Assert(driver).Equal(MockDriver{})
+		})
+		
+		g.It("Should fail on unknown driver", func() {
+			drivers := make(map[string]Driver)
+			
+			driver, err := getDriver("mock", drivers)
+			g.Assert(driver).Equal(nil)
+			g.Assert(err).Equal(ErrDriverNotRegistered)
 		})
 	})
 }
