b816caaa600580bb9b7889a998febf47f7e00f13
Romain Baugue <rb@capitaldata.fr>
2014-11-18 14:58:25 +0100

    Started MySQL driver

--- migration/driver/mysql/driver.go
+++ migration/driver/mysql/driver.go
@@ -1,23 +1,79 @@
 package mysql
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/elwinar/rambler/migration/driver"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"strings"
 )
 
 func init() {
-	driver.Register("mysql", Constructor)
+	driver.Register("mysql", NewDriver)
 }
 
-func Constructor(options string) (driver.Driver, error) {
-	return Driver{}, nil
+var (
+	ErrUnknownDatabase      = errors.New("invalid DSN or unreachable database")
+	ErrNotInitializedDriver = errors.New("driver not initialized")
+)
+
+type Driver struct {
+	db     *sqlx.DB
+	schema string
 }
 
-type Driver struct{}
+// NewDriver return a mysql driver that implements the driver interface
+func NewDriver(dsn string) (driver.Driver, error) {
+	return newDriver(dsn, sqlx.Connect)
+}
 
+type connecter func(string, string) (*sqlx.DB, error)
+
+func newDriver(dsn string, connect connecter) (*Driver, error) {
+	db, err := connect("mysql", dsn)
+	if err != nil {
+		return nil, ErrUnknownDatabase
+	}
+
+	lastSlash := strings.LastIndex(dsn, "/")
+	schema := dsn[lastSlash+1:]
+	firstQuestion := strings.Index(dsn, "?")
+	if firstQuestion != -1 {
+		schema = schema[:firstQuestion-1]
+	}
+
+	return &Driver{
+		db:     db,
+		schema: schema,
+	}, nil
+}
+
+// MigrationTableExists return wheter or not a tbale named migration exists in the
+// target schema.
 func (d Driver) MigrationTableExists() (bool, error) {
-	return false, nil
+	if d.db == nil {
+		return false, ErrNotInitializedDriver
+	}
+
+	var table struct {
+		Name string `db:'name'`
+	}
+	
+	err := d.db.Get(&table, fmt.Sprintf(`SELECT table_name as name FROM information_schema.tables WHERE table_schema = '%s' AND table_name = 'migrations'`, d.schema))
+
+	if err == sql.ErrNoRows {
+		return false, nil
+	}
+	
+	if err != nil {
+		return false, err
+	}
+	
+	return true, nil
 }
 
-func (d Driver) CreateMigrationTable() (error) {
+func (d Driver) CreateMigrationTable() error {
 	return nil
 }
--- migration/driver/mysql/driver_test.go
+++ migration/driver/mysql/driver_test.go
@@ -2,14 +2,35 @@ package mysql
 
 import (
 	. "github.com/franela/goblin"
+	"github.com/jmoiron/sqlx"
 	"testing"
 )
 
-func TestConstructor(t *testing.T) {
+var (
+	nilDriver *Driver
+)
+
+func TestNewDriver(t *testing.T) {
 	g := Goblin(t)
-	g.Describe("Constructor", func() {
-		g.It("Should reject invalid go-sql-driver DSN")
-		g.It("Should fail on unreachable database")
-		g.It("Should initialize the database handle")
+	g.Describe("NewDriver", func() {
+		g.It("Should reject invalid go-sql-driver DSN", func() {
+			d, err := newDriver("invalid", sqlx.Connect)
+			g.Assert(err).Equal(ErrUnknownDatabase)
+			g.Assert(d).Equal(nilDriver)
+		})
+
+		g.It("Should fail on unreachable database", func() {
+			d, err := newDriver("/unreachable", sqlx.Connect)
+			g.Assert(err).Equal(ErrUnknownDatabase)
+			g.Assert(d).Equal(nilDriver)
+		})
+
+		g.It("Should parse the DSN to get the schema", func() {
+			d, err := newDriver("/schema?parseTime=true", func(driver, dsn string) (*sqlx.DB, error) {
+				return &sqlx.DB{}, nil
+			})
+			g.Assert(err).Equal(nil)
+			g.Assert(d.schema).Equal("schema")
+		})
 	})
 }
