183e2b3959bfb0c61a31449c9786198d95e728fe
Romain Baugue <rb@capitaldata.fr>
2014-11-28 16:35:42 +0100

    Fixed configuration to avoid viper's embedded struct handling

--- lib/configuration.go
+++ lib/configuration.go
@@ -1,6 +1,13 @@
 package lib
 
 type Configuration struct {
-	Environment
+	Driver     string
+	Protocol   string
+	Host       string
+	Port       uint64
+	User       string
+	Password   string
+	Database   string
+	Migrations string
 	Environments map[string]RawEnvironment
 }
