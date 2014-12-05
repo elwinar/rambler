1566047722ad7efed5cba13391a8885efe32d6b8
Romain Baugue <rb@capitaldata.fr>
2014-12-05 10:09:25 +0100

    Added first tests for apply command

--- apply/command_test.go
+++ apply/command_test.go
@@ -1,6 +1,8 @@
 package apply
 
 import (
+	"github.com/elwinar/rambler/configuration"
+	"github.com/elwinar/rambler/migration"
 	. "github.com/franela/goblin"
 	"testing"
 )
@@ -11,9 +13,16 @@ func TestCommand(t *testing.T) {
 	var s MockService
 	var exists int
 	var creates int
+	
+	var newService serviceConstructor
+	var news int
+	
+	var env configuration.Environment = configuration.Environment{
+		Driver:    "mock",
+		Directory: "dir",
+	}
 
 	g.Describe("Command", func() {
-
 		g.BeforeEach(func() {
 			s.migrationTableExists = func() (bool, error) {
 				exists++
@@ -27,11 +36,21 @@ func TestCommand(t *testing.T) {
 
 			exists = 0
 			creates = 0
+			
+			newService = func(env configuration.Environment) (migration.Service, error) {
+				news++
+				return s, nil
+			}
+			
+			news = 0
 		})
 
 		g.It("Should initialize a new service", func() {
-
+			err := command(env, false, newService)
+			g.Assert(news).Equal(1)
+			g.Assert(err).Equal(nil)
 		})
+		
 		g.It("Should check for the migration table")
 		g.It("Should create the migration table if it does'nt exists")
 		g.It("Should list the already applied migrations")
