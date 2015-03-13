c2910242287e3a5472232e511df1b1b377a1d7e6
Romain Baugue <rb@capitaldata.fr>
2015-03-13 13:34:56 +0100

    Add a bootstrap method

---
+++ bootstrap.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+)
+
+// Bootstrap go the initialization job, and finish by setting the 
+// `service` global var that will be used by other commands.
+func Bootstrap(ctx *cli.Context) error {
+	return bootstrap(ctx.GlobalString("configuration"), ctx.GlobalString("environment"))
+}
+
+func bootstrap(configuration, environment string) error {
+	cfg, err := Load(configuration)
+	if err != nil {
+		return fmt.Errorf("unable to load configuration file: %s", err)
+	}
+
+	env, err := cfg.Env(environment)
+	if err != nil {
+		return fmt.Errorf("unable to load requested environment: %s", err)
+	}
+
+	srv, err := NewService(env)
+	if err != nil {
+		return fmt.Errorf("unable to initialize the migration service: %s", err)
+	}
+	
+	service = srv
+	return nil
+}
---
+++ rambler.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+)
+
+var app *cli.App
+var service *Service
+
+func init() {
+	log.SetFlags(0)
+	
+	app = cli.NewApp()
+	app.Name = "rambler"
+	app.Usage = "Migrate all the things!"
+	app.Version = "3"
+	app.Authors = []cli.Author{
+		{
+			Name: "Romain Baugue",
+			Email: "romain.baugue@elwinar.com",
+		},
+	}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "configuration, c",
+			Value: "rambler.json",
+			Usage: "path to the configuration file",
+		},
+		cli.StringFlag{
+			Name:  "environment, e",
+			Value: "default",
+			Usage: "set the working environment",
+		},
+	}
+	app.Before = Bootstrap
+	app.Commands = []cli.Command{
+		{
+			Name: "noop",
+			Usage: "no-op command for testing purpose",
+			Action: func(_ *cli.Context) { return },
+		},
+// 		{
+// 			Name:   "apply",
+// 			Usage:  "apply the next migration",
+// 			Flags: []cli.Flag{
+// 				cli.BoolFlag{
+// 					Name:  "all, a",
+// 					Usage: "Apply all migrations",
+// 				},
+// 			},
+// 			Action: Apply,
+// 		},
+// 		{
+// 			Name:   "reverse",
+// 			Usage:  "reverse the last migration",
+// 			Flags: []cli.Flag{
+// 				cli.BoolFlag{
+// 					Name:  "all, a",
+// 					Usage: "Reverse all migrations",
+// 				},
+// 			},
+// 			Action: Reverse,
+// 		},
+	}
+}
+
+func main() {
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Println(err)
+	}
+}
